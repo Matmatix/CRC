e9ce91cbd710f1fc49b10efd94d595091a1e9955
Matmatix <mattkinz96@gmail.com>
2017-10-13 14:00:45 -0500

    more progress on XORing

--- CRC.c
+++ CRC.c
@@ -1,42 +1,60 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
-#define MAXSIZE 8
+#define MAXSIZE 100
 
 char *int2bin(int, char *,int);
 
 int main(void)
 {
 	// Binary string based off input integer
-	char binaryString[MAXSIZE+1];
+	char binaryString[MAXSIZE+4];
+	int bi;
 	binaryString[MAXSIZE] = '\0';
-	// Binary string with appended value space
-	char appended[MAXSIZE+4];
+	char divisor[] = "1011";
+	// Value to append
+	char appended[] = "000";
 	char c;
 	int value = 0, i = 0;
-	printf("Enter an integer:\n");
+	printf("Enter a binary string:\n");
 	// Takes in any integer up to maxsize bits
 	while((c = getchar())!= '\n')
 	{
-			value = 10 * value + (c - '0');
+			binaryString[i++] = c;
 	}
 
-	printf("The input value is: %d\n", value);
-	int2bin(value, binaryString, MAXSIZE);
-	// Copy binaryString to larger array	
-	for(i = 0; i < MAXSIZE; i++)
-	{
-		appended[i+3] = binaryString[i];
-	}
-	// Put desired appended values (000 in this case) at end
-	for(i = 0; i<3; i++)
-	{
-		appended[i] = '0';
-	}
-	// Null terminate your stuff
-	appended[MAXSIZE+3] = '\0';
-	printf("The appended binary string is: %s\n", appended);
+	binaryString[i]='\0';
 
+	if(i < sizeof(divisor))
+	{
+		printf("Binary string is too small.\n");
+		return 0;
+	}
+
+	printf("The input value is: %s\n", binaryString);
+	//int2bin(value, binaryString, MAXSIZE);
+
+	// Copy binaryString to larger array
+	strcat(binaryString, appended);
+	printf("The appended binary string is: %s\n", binaryString);
+
+	int j;
+	for(j=0; j< i-(sizeof(divisor)-1);j++)
+	{
+		while(binaryString[j] == '0'){
+			j++;
+		}
+		printf("%c%c%c%c XOR %s\n", binaryString[j], binaryString[j+1], binaryString[j+2], binaryString[j+3],divisor);
+		binaryString[j] 	= (binaryString[j]-'0')^divisor[0];
+		binaryString[j+1] = (binaryString[j+1]-'0')^divisor[1];
+		binaryString[j+2] = (binaryString[j+2]-'0')^divisor[2];
+		binaryString[j+3] = (binaryString[j+3]-'0')^divisor[3];
+		//binaryString[j+4] = (binaryString[j+4]-'0')^divisor[4];
+		//printf("%s\n", binaryString);
+	}
+	printf("Final value: %s\n", binaryString);
+	return 0;
 }
 
 char *int2bin(int a, char *buffer, int buf_size) {
