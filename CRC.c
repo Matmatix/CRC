341b6be74a32cecb7ad088ba68f74b9dda56308d
MakesCents <jaredfunk28@gmail.com>
2017-11-18 17:21:38 -0600

    Made most of the changes prior to dynamic programming

--- CRC.c
+++ CRC.c
@@ -3,17 +3,22 @@
 #include <stdlib.h>
 #include <string.h>
 #include <time.h>
+typedef int bool;
+#define true 1
+#define false 0
 #define MAXSIZE 200
+char divisor[] = "10011";
 char* bitStreamGen(int len);
-char* CRC(char binaryString[], int inLen, char divisor[], int divLen);
+char* CRC(char binaryString[], int inLen);
 char* interface(char binaryString[]);
+bool checkSuccess(char binaryString[]);
 int main(void)
 {
 	// Binary string based off input integer
 	int i = 0;
 	char *result;
 	char binaryString[MAXSIZE + 1];
-	char appendedString[MAXSIZE + 1];	
+	char appendedString[MAXSIZE + 5];	
 
 	srand(time(NULL));
 	for(i = 0; i < 1; i++)
@@ -23,11 +28,16 @@ int main(void)
 		printf("%s\r\n", appendedString);
 		result = interface(binaryString);
 		printf("%s is remainder\r\n", result);
-		printf("%s\r\n", appendedString);
 		strcat(appendedString, result);
-		result = interface(appendedString);
-		printf("%s is remainder\r\n", result);
-
+		printf("%s is result\r\n", appendedString);
+		if(checkSuccess(appendedString) == true)
+		{
+			printf("Success!\r\n");
+		}
+		else
+		{
+			printf("Failure :(\r\n");
+		}
 	}
 	free(result);
 }
@@ -35,10 +45,7 @@ int main(void)
 char* interface(char binaryString[])
 {
 	char *result;
-	char divisor[] = "10011";
 	int i = 0;
-
-	strcpy(binaryString, bitStreamGen(MAXSIZE));
 	i = MAXSIZE;
 	//Can't really check anything if smaller than divisor
 	if(i < sizeof(divisor))
@@ -48,11 +55,77 @@ char* interface(char binaryString[])
 	}
 
 	clock_t begin = clock();
-	result = CRC(binaryString, i, divisor, sizeof(divisor) - 1);
+	result = CRC(binaryString, i);
 	clock_t end = clock();
 	printf("%.3f ms\r\n", (double)(end - begin)*1000/CLOCKS_PER_SEC);
 	return result; 
 }
+
+
+char* CRC(char binaryString[], int inLen)
+{
+	int i = 0;
+	int j = 0;
+	char* result;
+	int divLen = (sizeof(divisor) - 1);
+	result = (char*) malloc(5);
+	result[4] = '\0';
+	// For display/testing purposes
+	int displayIndex = 0;
+	while(inLen-i >= divLen)
+	{
+		//Skip if at a zero
+		while(binaryString[i] == '0')
+			i++;
+		if(inLen - i < divLen)
+			break;
+		///* Used for display purpose only
+		printf("%s\r\n", binaryString);
+		
+		displayIndex = 0;
+		while(displayIndex < i)
+		{
+			printf(" ");
+			displayIndex++;
+		}
+		printf("%s\r\n", divisor, i, inLen, divLen);
+	
+		//*/ Remove above section for good performance
+		// Run through each current val + next divLen spots
+		for(j = 0; j < divLen; j++)
+		{
+
+			binaryString[j+i] = (binaryString[j+i] - '0') ^ divisor[j];
+		}
+		i++;
+	}
+	printf("\r\n");
+	result[0] = binaryString[inLen-4];
+	result[1] = binaryString[inLen-3];
+	result[2] = binaryString[inLen-2];
+	result[3] = binaryString[inLen-1];
+	return result;
+}
+bool checkSuccess(char binaryString[])
+{
+	char* result;
+	//result = (char*) malloc(5);
+	//result[4] = '\0';
+	char* inResult;
+	inResult = (char*) malloc(5);
+	inResult[4] = '\0';
+	inResult[0] = binaryString[MAXSIZE];
+	inResult[1] = binaryString[MAXSIZE+1];
+	inResult[2] = binaryString[MAXSIZE+2];
+	inResult[3] = binaryString[MAXSIZE+3];
+	result = CRC(binaryString, (MAXSIZE - 3));
+	printf("input: %s\r\n", inResult);
+	printf("output: %s\r\n", result);
+	return result[0] == binaryString[MAXSIZE-1];
+}
+
+
+
 
 
 char *bitStreamGen(int len)
@@ -66,55 +139,4 @@ char *bitStreamGen(int len)
 	output[len] = '\0';
 	return output;
 
-}
-
-
-char* CRC(char binaryString[], int inLen, char divisor[], int divLen)
-{
-	int i = 0;
-	int j = 0;
-	char* result;
-	result = (char*) malloc(4);
-	result[3] = '\0';
-	printf("Running...");
-	// Remove displayIndex
-	//int displayIndex = 0;
-	while(inLen-i >= divLen)
-	{
-
-		//Skip if at a zero
-		while(binaryString[i] == '0')
-			i++;
-		//Need a better solution for this
-		//Should I just use an infinite loop and rely on this check
-		//For exiting?
-		if(inLen - i < divLen)
-			break;
-		///* Used for display purpose only
-		//printf("%s\r\n", binaryString);
-		/*
-		displayIndex = 0;
-		while(displayIndex < i)
-		{
-			//printf(" ");
-			displayIndex++;
-		}
-		printf("%s\r\n", divisor, i, inLen, divLen);
-	*/
-
-
-		//*/ Remove above section for good performance
-		// Run through each current val + next divLen spots
-		for(j = 0; j < divLen; j++)
-		{
-
-			binaryString[j+i] = (binaryString[j+i] - '0') ^ divisor[j];
-		}
-		i++;
-	}
-	printf("\r\n");
-	result[0] = binaryString[inLen-3];
-	result[1] = binaryString[inLen-2];
-	result[2] = binaryString[inLen-1];
-	return result;
 }
