19e3b8301222030bfcb6d5795b8c42cbfb526dd6
MakesCents <jaredfunk28@gmail.com>
2017-11-20 20:55:30 -0600

    Attempts at getting this to work, should work if we shift the polynomial instead of the remainder

--- CRCtemp.cpp
+++ CRCtemp.cpp
@@ -1,10 +1,15 @@
+#include <iostream>
+#include <bitset>
 #include <stdio.h>
 #define POLYNOMIAL 0b1011  /* 11011 followed by 0's */
 
+using namespace std;
+int bitCount(unsigned int n);
 int crcNaive(int message);
 int main()
 {
-    printf("%x", crcNaive(0xA2AAB95));
+    std::cout << std::bitset<28>(0xA2AAB9) << endl;
+    printf("%x", crcNaive(0xA2AAB9));
 }
 int
 crcNaive(int message)
@@ -16,16 +21,18 @@ crcNaive(int message)
      * Initially, the dividend is the remainder.
      */
     remainder = message;
-
+    int const bitSize = bitCount(message);
     /*
      * For each bit position in the message....
      */
-    for (int bit = 8; bit > 0; --bit)
+    for (int bit = bitSize; bit > 0; --bit)
     {
         /*
          * If the uppermost bit is a 1...
          */
-        if (remainder & 0x80)
+        std::cout << std::bitset<28>(remainder) << endl;
+       printf("1011\n");
+        if (remainder & (1 << (bitSize - 1)))
         {
             /*
              * XOR the previous remainder with the divisor.
@@ -42,6 +49,15 @@ crcNaive(int message)
     /*
      * Return only the relevant bits of the remainder as CRC.
      */
-    return (remainder >> 4);
+    return (remainder >> 3);
 
 }   /* crcNaive() */
+ int bitCount(unsigned int n) {
+
+    int counter = 0;
+    while(n) {
+        counter += n % 2;
+        n >>= 1;
+    }
+    return counter;
+ }
